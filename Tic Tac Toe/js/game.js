4fbf2513791d9b947334e209973e1eb590001398
jomoke814 <jcullen814@gmail.com>
2022-11-30 19:12:41 +0300

    Adding functions for gameborad, adding marks, switch turns and adding marker

--- Tic Tac Toe/js/game.js
+++ Tic Tac Toe/js/game.js
@@ -1,1 +1,46 @@
-console.log('Hello John, I am Js')
+const cellElements = document.querySelectorAll('[data-cell]')
+const x_Mark = 'x'
+const circle_Mark = 'circle'
+let circleTurn
+
+
+let gameBoardModule = (function () {
+    let gameBoard = [];
+    return {};
+})();
+
+let displayControlModule = (function () {
+    let test = () => {console.log('Testing! Testing the Function')}
+    return{test};
+})();
+
+function createPlayer(Player1, Player2) {
+    let getPlayerName = () => {}
+    return {getPlayerName, Player1, Player2}
+}
+
+//adding click event for gameBoard
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once: true})
+})
+
+//adding mark from browser
+function handleClick(e) {
+    const cell = e.target
+    const currentMark = circleTurn ? circle_Mark : x_Mark
+    makeMaker(cell, currentMark)
+    // check winner
+    //check for draw
+    // switch turns
+    switchTurns()
+}
+
+//add a mark o or x
+function makeMaker(cell, currentMark) {
+    cell.classList.add(currentMark)
+}
+
+// switch turn to x or x
+function switchTurns() {
+    circleTurn = !circleTurn
+}

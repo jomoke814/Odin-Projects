242277fe40bfef332afa33f64fa557c64f1d256d
jomoke814 <jcullen814@gmail.com>
2022-12-01 15:23:31 +0300

    Adding hover function effect

--- Tic Tac Toe/js/game.js
+++ Tic Tac Toe/js/game.js
@@ -1,31 +1,28 @@
-const cellElements = document.querySelectorAll('[data-cell]')
+const cellElements = document.querySelectorAll('[data-cell]');
+const gameBoard = document.querySelector('.game-board');
 const x_Mark = 'x'
 const circle_Mark = 'circle'
 let circleTurn
 
 
 let gameBoardModule = (function () {
-    let gameBoard = [];
+    // let gameBoard = [];
+    
     return {};
 })();
-
+//this a module
 let displayControlModule = (function () {
-    let test = () => {console.log('Testing! Testing the Function')}
-    return{test};
-})();
+    function startGame() {
 
-function createPlayer(Player1, Player2) {
-    let getPlayerName = () => {}
-    return {getPlayerName, Player1, Player2}
-}
+        //adding click event for gameBoard
+        cellElements.forEach(cell => {
+            cell.addEventListener('click', handleClick, {once: true})
+        })
+        setBoardHover()
+    }
 
-//adding click event for gameBoard
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, {once: true})
-})
-
-//adding mark from browser
-function handleClick(e) {
+    //adding mark from browser
+    function handleClick(e) {
     const cell = e.target
     const currentMark = circleTurn ? circle_Mark : x_Mark
     makeMaker(cell, currentMark)
@@ -33,6 +30,15 @@ function handleClick(e) {
     //check for draw
     // switch turns
     switchTurns()
+    setBoardHover()
+    }
+    return{startGame, handleClick};
+})();
+displayControlModule.startGame()
+
+function createPlayer(Player1, Player2) {
+    let getPlayerName = () => {}
+    return {getPlayerName, Player1, Player2}
 }
 
 //add a mark o or x
@@ -43,4 +49,15 @@ function makeMaker(cell, currentMark) {
 // switch turn to x or x
 function switchTurns() {
     circleTurn = !circleTurn
+}
+
+//adding hover effects gameBoard
+function setBoardHover() {
+    gameBoard.classList.remove(x_Mark)
+    gameBoard.classList.remove(circle_Mark)
+    if(circleTurn) {
+        gameBoard.classList.add(circle_Mark)
+    } else {
+        gameBoard.classList.add(x_Mark)
+    }
 }

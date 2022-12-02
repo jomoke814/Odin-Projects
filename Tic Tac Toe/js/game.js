bad82d7e425d89870801f76528bb07944dea050f
jomoke814 <jcullen814@gmail.com>
2022-12-02 17:53:58 +0300

    Adding Start btn on html and creating player factory function in js files

--- Tic Tac Toe/js/game.js
+++ Tic Tac Toe/js/game.js
@@ -1,28 +1,73 @@
 const cellElements = document.querySelectorAll('[data-cell]');
 const gameBoard = document.querySelector('.game-board');
+const startButton = document.querySelector('.start')
 const x_Mark = 'x'
 const circle_Mark = 'circle'
 let circleTurn
 
+
+//Creating players
+let createPlayer = () => {
+    for(let i = 0; i < 4; i++) {
+        if(gameBoardModule.playerArray.length >= 0) {
+            gameBoardModule.makePlayerMove();
+            break;
+        }else if(gameBoardModule.playerArray.length == 0) {
+            let playerName = prompt("Whats your name Player1?");
+            if(playerName == '' || playerName == null) {
+                alert("Sorry, Name cannot be blank!");
+                continue;
+            }
+
+            let playerNumber = 1;
+            let assignSymbol = x_Mark;
+            alert("Your player1 assigned X");
+            gameBoardModule.playerArray.push(playerName,playerNumber,assignSymbol);
+        }else if(gameBoardModule.playerArray.length !== 0 ) {
+            let playerName = prompt("Whats your name Player2?");
+            if(playerName == '' || playerName == null) {
+                alert("Sorry, Name cannot be blank!");
+                continue;
+            }
+
+            let playerNumber = 2;
+            let assignSymbol = circle_Mark;
+            alert("Your player1 assigned circle");
+            gameBoardModule.playerArray.push(playerName,playerNumber,assignSymbol);
+        }
+    }
+};
 
 let gameBoardModule = (function () {
     // let gameBoard = [];
     
     return {};
 })();
+
+//adding hover effects gameBoard
+const setBoardHover = () => {
+    gameBoard.classList.remove(x_Mark)
+    gameBoard.classList.remove(circle_Mark)
+    if(circleTurn) {
+        gameBoard.classList.add(circle_Mark)
+    } else {
+        gameBoard.classList.add(x_Mark)
+    }
+};
+
 //this a module
 let displayControlModule = (function () {
-    function startGame() {
-
+    const startHover = () => {
+        circleTurn = false
         //adding click event for gameBoard
         cellElements.forEach(cell => {
             cell.addEventListener('click', handleClick, {once: true})
         })
         setBoardHover()
-    }
+    };
 
     //adding mark from browser
-    function handleClick(e) {
+    const handleClick = (e) => {
     const cell = e.target
     const currentMark = circleTurn ? circle_Mark : x_Mark
     makeMaker(cell, currentMark)
@@ -31,33 +76,18 @@ let displayControlModule = (function () {
     // switch turns
     switchTurns()
     setBoardHover()
-    }
-    return{startGame, handleClick};
+    };
+    return{startHover, handleClick};
 })();
-displayControlModule.startGame()
-
-function createPlayer(Player1, Player2) {
-    let getPlayerName = () => {}
-    return {getPlayerName, Player1, Player2}
-}
+displayControlModule.startHover()
 
 //add a mark o or x
-function makeMaker(cell, currentMark) {
+const makeMaker = (cell, currentMark) => {
     cell.classList.add(currentMark)
-}
+};
 
 // switch turn to x or x
-function switchTurns() {
+const switchTurns = () => {
     circleTurn = !circleTurn
-}
+};
 
-//adding hover effects gameBoard
-function setBoardHover() {
-    gameBoard.classList.remove(x_Mark)
-    gameBoard.classList.remove(circle_Mark)
-    if(circleTurn) {
-        gameBoard.classList.add(circle_Mark)
-    } else {
-        gameBoard.classList.add(x_Mark)
-    }
-}
